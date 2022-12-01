b7e86390573ed184d965de142abd5f4707261bfb
hakesson <henrik@akesson.mobi>
2022-12-01 14:33:22 +0100

    Use Default value instead of wrapping in Option

--- src/app/keyed_signal.rs
+++ src/app/keyed_signal.rs
@@ -10,21 +10,25 @@ use leptos::{
 ///
 /// Only when the key changes, when it's not equal to previous value,
 /// the lookup function is called.
+///
+/// The value needs to implement Default, if it happens to be something
+/// that doesn't have a Default implementation, then wrap it in an Option
+/// since it implements a Default value of None.
 pub struct KeyedSignal<Key, Val>
 where
     Key: 'static + PartialEq + Clone + Debug,
-    Val: 'static,
+    Val: 'static + Default,
 {
     inner: KeyedSignalInner<Key, Val>,
-    pub value: ReadSignal<Option<Val>>,
+    pub value: ReadSignal<Val>,
 }
 
 impl<Key, Val> KeyedSignal<Key, Val>
 where
     Key: 'static + PartialEq + Clone + Debug,
-    Val: 'static + Clone,
+    Val: 'static + Clone + Default,
 {
-    pub fn get(&self) -> Option<Val> {
+    pub fn get(&self) -> Val {
         self.value.get()
     }
     pub fn get_key(&self) -> Key {
@@ -43,8 +47,8 @@ pub fn create_keyed_signal<Key, Val, F, Fu>(
 ) -> KeyedSignal<Key, Val>
 where
     Key: 'static + PartialEq + Clone + Debug,
-    Val: 'static,
-    F: Fn(Key, WriteSignal<Option<Val>>) -> Fu + 'static,
+    Val: 'static + Default,
+    F: Fn(Key, WriteSignal<Val>) -> Fu + 'static,
     Fu: Future<Output = ()> + 'static,
 {
     let (value, inner) = create_key_signal_inner(cx, key, lookup);
@@ -60,29 +64,29 @@ where
 struct KeyedSignalInner<Key, Val>
 where
     Key: 'static + PartialEq + Clone + Debug,
-    Val: 'static,
+    Val: 'static + Default,
 {
     key: Memo<Key>,
-    set_value: WriteSignal<Option<Val>>,
+    set_value: WriteSignal<Val>,
 
-    action_fn: Rc<dyn Fn(Key, WriteSignal<Option<Val>>) -> Pin<Box<dyn Future<Output = ()>>>>,
+    action_fn: Rc<dyn Fn(Key, WriteSignal<Val>) -> Pin<Box<dyn Future<Output = ()>>>>,
 }
 
 fn create_key_signal_inner<Key, Val, F, Fu>(
     cx: Scope,
     key: impl Fn() -> Key + 'static,
     lookup: F,
-) -> (ReadSignal<Option<Val>>, KeyedSignalInner<Key, Val>)
+) -> (ReadSignal<Val>, KeyedSignalInner<Key, Val>)
 where
     Key: 'static + PartialEq + Clone + Debug,
-    Val: 'static,
-    F: Fn(Key, WriteSignal<Option<Val>>) -> Fu + 'static,
+    Val: 'static + Default,
+    F: Fn(Key, WriteSignal<Val>) -> Fu + 'static,
     Fu: Future<Output = ()> + 'static,
 {
     let key = create_memo(cx, move |_| key());
-    let (value, set_value) = create_signal(cx, None);
+    let (value, set_value) = create_signal(cx, Val::default());
 
-    let action_fn = Rc::new(move |input: Key, value: WriteSignal<Option<Val>>| {
+    let action_fn = Rc::new(move |input: Key, value: WriteSignal<Val>| {
         let input = input.clone();
         let fut = lookup(input, value);
         Box::pin(async move { fut.await }) as Pin<Box<dyn Future<Output = ()>>>
